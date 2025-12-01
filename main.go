0b75896b00cc3bfe7f6ad8fe9ba418093e08f5e8
odeandialamsyah <odeandialamsyah@gmail.com>
2025-12-01 14:54:45 +0700

    second

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	routes "be-geojson/route"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders:  "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:  "GET,POST,PUT,DELETE,OPTIONS",
+	}))
 
 	config.ConnectMongo()
 
